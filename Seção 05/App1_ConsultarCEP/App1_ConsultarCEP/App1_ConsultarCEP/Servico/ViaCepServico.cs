eeea6ebca9f1b0b1c21dd7cd5a0da12699520c33
Felipe Lima <felip3.fl@gmail.com>
2019-11-02 19:14:33 -0300

    First Commit

---
+++ Seção 05/App1_ConsultarCEP/App1_ConsultarCEP/App1_ConsultarCEP/Servico/ViaCepServico.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1_ConsultarCEP.Servico
+{
+    public class ViaCepServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+
+    }
+}
