f969b35ef3cd87a0efdb4ce7b02e49d9e208a189
Felip3FL <felip3.fl@gmail.com>
2019-11-17 14:28:48 -0300

    44. [DEMO] App - Tipos de Página - Android - Demonstração

--- Seção 06/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Master/Master.xaml.cs
+++ Seção 06/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Master/Master.xaml.cs
@@ -20,16 +20,19 @@ namespace App02_TipoPaginaXF.TipoPagina.Master
         private void MudarPagina1(object sender, EventArgs args)
         {
             Detail = new Navigation.Pagina1();
+            IsPresented = false;
         }
 
         private void MudarPagina2(object sender, EventArgs args)
         {
             Detail = new Navigation.Pagina2();
+            IsPresented = false;
         }
 
         private void MudarPagina3(object sender, EventArgs args)
         {
             Detail = new Conteudo();
+            IsPresented = false;
         }
 
     }
