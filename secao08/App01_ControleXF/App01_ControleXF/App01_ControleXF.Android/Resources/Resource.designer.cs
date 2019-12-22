484dad11ccd5c76782c9be73b92f27cfd02800c1
Felip3FL <felip3.fl@gmail.com>
2019-12-22 10:39:36 -0300

    85. [ESTUDO] WebView - Correção navegação com o Android 9

--- secao08/App01_ControleXF/App01_ControleXF/App01_ControleXF.Android/Resources/Resource.designer.cs
+++ secao08/App01_ControleXF/App01_ControleXF/App01_ControleXF.Android/Resources/Resource.designer.cs
@@ -5796,6 +5796,9 @@ namespace App01_ControleXF.Droid
 			// aapt resource value: 0x7F070076
 			public const int tooltip_frame_light = 2131165302;
 			
+			// aapt resource value: 0x7F070077
+			public const int usb = 2131165303;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
