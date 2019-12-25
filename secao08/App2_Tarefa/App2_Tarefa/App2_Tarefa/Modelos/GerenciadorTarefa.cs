d01fd7ca77198abd1dbd6ddb4d7006120df08fb7
Felip3FL <felip3.fl@gmail.com>
2019-12-25 19:04:29 -0300

    95. [PROJETO] Modelo e Armazenamento

---
+++ secao08/App2_Tarefa/App2_Tarefa/App2_Tarefa/Modelos/GerenciadorTarefa.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2_Tarefa.Modelos
+{
+    public class GerenciadorTarefa
+    {
+        public List<Tarefa> Lista { get; set; }
+
+        public void Salvar(Tarefa tarefa)
+        {
+            Lista.Add(tarefa);
+            
+            SalvarNoProperties(Lista);
+        }
+
+        public void Finalizar(int Index, Tarefa tarefa)
+        {
+            Lista.RemoveAt(Index);
+            Lista.Add(tarefa);
+            SalvarNoProperties(Lista);
+        }
+
+        public void Deletar(Tarefa tarefa)
+        {
+            Lista.Remove(tarefa);
+            SalvarNoProperties(Lista);
+        }
+
+        public List<Tarefa> Listagem()
+        {
+            return ListagemNoProperties();
+        }
+
+        private List<Tarefa> ListagemNoProperties()
+        {
+            if (App.Current.Properties.ContainsKey("Tarefas"))
+            {
+                return (List<Tarefa>)App.Current.Properties["Tarefas"];
+            }
+
+            return new List<Tarefa>();
+        }
+
+        private void SalvarNoProperties(List<Tarefa> Lista)
+        {
+            if (App.Current.Properties.ContainsKey("Tarefas"))
+            {
+                App.Current.Properties.Remove("Tarefas");
+            }
+
+            App.Current.Properties.Add("Tarefas", Lista);
+        }
+
+    }
+}
