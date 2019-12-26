0e8f819b6f20bcda4fcc58c3d1814a6f11257e32
Felip3FL <felip3.fl@gmail.com>
2019-12-26 20:12:02 -0300

    101. [PROJETO] Tela "Início" - Criando Listagem - Parte 2

--- secao08/App2_Tarefa/App2_Tarefa/App2_Tarefa/Telas/Inicio.xaml.cs
+++ secao08/App2_Tarefa/App2_Tarefa/App2_Tarefa/Telas/Inicio.xaml.cs
@@ -27,8 +27,31 @@ namespace App2_Tarefa.Telas
 
         public void LinhaStackLayout(Tarefa tarefa)
         {
-            Label TxtTarefa = new Label() { VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Fill,  Text = tarefa.Nome};
+            Image Delete = new Image() { VerticalOptions = LayoutOptions.Center, Source = ImageSource.FromFile("Delete.png") };
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                Delete.Source = ImageSource.FromFile("Resources/Delete.png");
+            }
 
+            Image Prioridade = new Image() { VerticalOptions = LayoutOptions.Center, Source = ImageSource.FromFile(tarefa.Prioridade + ".png") };
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                Prioridade.Source = ImageSource.FromFile("Resources/" + tarefa.Prioridade + ".png");
+            }
+
+            View StackCentral = null;
+            if (tarefa.DataFinalizacao == null)
+            {
+                StackCentral = new Label() { VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Fill, Text = tarefa.Nome };
+            }
+            else
+            {
+                StackCentral = new StackLayout() { VerticalOptions = LayoutOptions.Center, Spacing = 0, HorizontalOptions = LayoutOptions.FillAndExpand };
+                ((StackLayout)StackCentral).Children.Add(new Label() { Text = tarefa.Nome, TextColor = Color.Gray });
+                ((StackLayout)StackCentral).Children.Add(new Label() { Text = "Finalizado em " + tarefa.DataFinalizacao.ToString("dd/MM/yyyy - hh:mm") + "h", TextColor = Color.Gray, FontSize = 10 });
+            }
+
+           
             Image check = new Image() { VerticalOptions = LayoutOptions.Center, Source = ImageSource.FromFile("CheckOff.png")};
             if (Device.RuntimePlatform == Device.UWP)
             {
@@ -38,6 +61,11 @@ namespace App2_Tarefa.Telas
             StackLayout Linha = new StackLayout() { Orientation = StackOrientation.Horizontal, Spacing =15 };
 
             Linha.Children.Add(check);
+            Linha.Children.Add(StackCentral);
+            Linha.Children.Add(Prioridade);
+            Linha.Children.Add(Delete);
+
+            SLTarefas.Children.Add(Linha);
         }
     }
 }
