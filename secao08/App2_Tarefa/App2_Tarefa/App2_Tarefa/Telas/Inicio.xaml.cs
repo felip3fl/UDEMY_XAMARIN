ee9764674f01c08cff0c55dcd6b2f84deda74974
Felip3FL <felip3.fl@gmail.com>
2019-12-26 19:59:58 -0300

    100. [PROJETO] Tela "Início" - Criando Listagem - Parte 1

--- secao08/App2_Tarefa/App2_Tarefa/App2_Tarefa/Telas/Inicio.xaml.cs
+++ secao08/App2_Tarefa/App2_Tarefa/App2_Tarefa/Telas/Inicio.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App2_Tarefa.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,28 @@ namespace App2_Tarefa.Telas
         public Inicio()
         {
             InitializeComponent();
+
+            DataHoje.Text = DateTime.Now.DayOfWeek.ToString() + ", " + DateTime.Now.ToString("dd/MM");
         }
 
         public void ActionGoCadastro(object sender, EventArgs args)
         {
             Navigation.PushAsync(new Cadastro());
+        }
+
+        public void LinhaStackLayout(Tarefa tarefa)
+        {
+            Label TxtTarefa = new Label() { VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Fill,  Text = tarefa.Nome};
+
+            Image check = new Image() { VerticalOptions = LayoutOptions.Center, Source = ImageSource.FromFile("CheckOff.png")};
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                check.Source = ImageSource.FromFile("Resources/CheckOff.png");
+            }
+
+            StackLayout Linha = new StackLayout() { Orientation = StackOrientation.Horizontal, Spacing =15 };
+
+            Linha.Children.Add(check);
         }
     }
 }
