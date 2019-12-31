d57fd8519bb6f665cef7cbeaea7afbaa81716bda
Felip3FL <felip3.fl@gmail.com>
2019-12-31 12:04:19 -0300

    146. [ESTUDO/PROJETO] Entendendo o DependencyService

---
+++ secao11/App1_Vagas/App1_Vagas/App1_Vagas/Banco/DataBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using App1_Vagas.Modelos;
+
+namespace App1_Vagas.Banco
+{
+    public class DataBase
+    {
+        private SQLiteConnection _conexao;
+
+        public DataBase()
+        {
+            _conexao = new SQLiteConnection();
+        }
+
+        public void Cadastro(Vaga vaga)
+        {
+            
+        }
+
+        public List<Vaga> Consultar()
+        {
+            return null;
+        }
+
+        public Vaga ObterVagaPorId(int id)
+        {
+            return null;
+        }
+
+        public void Atualizacao(Vaga vaga)
+        {
+
+        }
+
+        public void Exclusao(int id)
+        {
+
+        }
+
+
+
+    }
+}
