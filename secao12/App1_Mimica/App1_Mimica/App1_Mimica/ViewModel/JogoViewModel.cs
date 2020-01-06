c932b1adbbe99be427695ff806e4ff7f040516a5
Felip3FL <felip3.fl@gmail.com>
2020-01-05 22:15:00 -0300

    187. [ESTUDO/PROJETO] MVVM - Nome do Grupo

--- secao12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/JogoViewModel.cs
+++ secao12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/JogoViewModel.cs
@@ -11,6 +11,7 @@ namespace App1_Mimica.ViewModel
     {
         public Grupo Grupo { get; set; }
         public string NomeGrupo { get; set; }
+        public string NumeroGrupo { get; set; }
 
         private byte _PalavraPontuacao;
         public byte PalavraPontuacao { get { return _PalavraPontuacao; } set { _PalavraPontuacao = value; OnPropertyChanged("PalavraPontuacao"); }}
@@ -39,6 +40,15 @@ namespace App1_Mimica.ViewModel
         {
             Grupo = grupo;
             NomeGrupo = grupo.Nome;
+
+            if(grupo == Armazenamento.Armazenamento.Jogo.Grupo1)
+            {
+                NumeroGrupo = "Grupo 1";
+            }
+            else
+            {
+                NumeroGrupo = "Grupo 2";
+            }
 
             IsVisibleContainerContagem = false;
             IsVisibleContainerIniciar = false;
