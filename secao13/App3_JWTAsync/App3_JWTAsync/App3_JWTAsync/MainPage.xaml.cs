4da1ab9a2af6d3b25b2e4b756c9da9fa759a3372
Felip3FL <felip3.fl@gmail.com>
2020-01-16 21:33:05 -0300

    224. [ESTUDO/PROJETO] NossoChat - JWT - REST - Verificar Token

---
+++ secao13/App3_JWTAsync/App3_JWTAsync/App3_JWTAsync/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3_JWTAsync
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public async void GetTokenAction(object sender, EventArgs args)
+        {
+            string resultado = await JWTService.GetToken(nome.Text, password.Text);
+            LblToken.Text = resultado;
+
+        }
+        public async void VerificarAction(object sender, EventArgs args)
+        {
+            string resultado = await JWTService.Verificar();
+            LblResultado.Text = resultado;
+        }
+    }
+}
